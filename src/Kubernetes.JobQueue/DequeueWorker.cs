e2e6b7ed80fb13579ec892da5c4b90af68f08ffd
damianfadri <damianfadri@gmail.com>
2025-05-27 22:14:04 +0800

    add initial implementation

---
+++ src/Kubernetes.JobQueue/DequeueWorker.cs
@@ -0,0 +1,65 @@
+﻿using k8s;
+using k8s.Models;
+using Microsoft.Extensions.Options;
+
+namespace Kubernetes.JobQueue;
+
+public class DequeueWorker(
+    k8s.Kubernetes client,
+    IOptions<KubernetesOptions> options) : ThreadedHostedService
+{
+    protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+    {
+        var jobList = client.BatchV1.ListNamespacedJobWithHttpMessagesAsync(
+            namespaceParameter: "default",
+            labelSelector: "jobs/queue-enabled=true",
+            watch: true,
+            cancellationToken: stoppingToken);
+
+        await foreach (var (type, job) in jobList.WatchAsync<V1Job, V1JobList>(cancellationToken: stoppingToken))
+        {
+            if (type != WatchEventType.Modified)
+            {
+                continue;
+            }
+
+            try
+            {
+                var deployedJobs = await client.BatchV1.ListNamespacedJobAsync(
+                    namespaceParameter: "default",
+                    labelSelector: "jobs/queue-enabled=true",
+                    cancellationToken: stoppingToken);
+
+                var upcomingJob = deployedJobs.Items
+                    .Where(deployedJob => !deployedJob.IsDone())
+                    .OrderBy(deployedJob => deployedJob.Metadata.CreationTimestamp)
+                    .FirstOrDefault();
+
+                if (upcomingJob == null)
+                {
+                    continue;
+                }
+
+                if (!upcomingJob.Spec.Suspend.HasValue || !upcomingJob.Spec.Suspend.Value)
+                {
+                    continue;
+                }
+
+                var patch = new V1Patch(new SuspendPatch(false), V1Patch.PatchType.MergePatch);
+                await client.BatchV1.PatchNamespacedJobAsync(
+                    body: patch,
+                    name: upcomingJob.Metadata.Name,
+                    namespaceParameter: "default",
+                    cancellationToken: stoppingToken);
+
+                Console.WriteLine($"{upcomingJob.Metadata.Name} is next, running.");
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Dequeue error");
+                Console.WriteLine(ex.ToString());
+            }
+        }
+    }
+}
---
+++ src/Kubernetes.JobQueue/EnqueueWorker.cs
@@ -0,0 +1,50 @@
+﻿using k8s;
+using k8s.Models;
+using Microsoft.Extensions.Options;
+
+namespace Kubernetes.JobQueue;
+
+public class EnqueueWorker(k8s.Kubernetes client, IOptions<KubernetesOptions> options) : ThreadedHostedService
+{
+    protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+    {
+        while (!stoppingToken.IsCancellationRequested)
+        {
+            var jobList = client.BatchV1.ListNamespacedJobWithHttpMessagesAsync(
+                namespaceParameter: options.Value.Namespace,
+                labelSelector: options.Value.Selector,
+                watch: true,
+                cancellationToken: stoppingToken);
+
+            await foreach (var (type, job) in 
+                jobList.WatchAsync<V1Job, V1JobList>(cancellationToken: stoppingToken))
+            {
+                if (type != WatchEventType.Added)
+                {
+                    continue;
+                }
+
+                if (job.IsDone())
+                {
+                    continue;
+                }
+
+                try
+                {
+                    var patch = new V1Patch(new SuspendPatch(true), V1Patch.PatchType.MergePatch);
+
+                    await client.BatchV1.PatchNamespacedJobAsync(
+                        body: patch,
+                        name: job.Metadata.Name,
+                        namespaceParameter: options.Value.Namespace,
+                        cancellationToken: stoppingToken);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Enqueue error");
+                    Console.WriteLine(ex.ToString());
+                }
+            }
+        }
+    }
+}
